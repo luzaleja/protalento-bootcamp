007e83c146065eb5c3aa4204806b471b66ef4c13
luzaleja <luzaleja6@gmail.com>
2022-05-12 19:16:35 -0500

    Clase54
    AdminDeConexiones

--- app-dao/src/main/java/ar/com/educacionit/dao/jdbc/AdministradorDeConexiones.java
+++ app-dao/src/main/java/ar/com/educacionit/dao/jdbc/AdministradorDeConexiones.java
@@ -10,10 +10,10 @@ public class AdministradorDeConexiones {
 
 	public static Connection obtenerConexion() throws GenericException {
 		
-		String url = "jdbc:postgresql://ec2-52-203-164-61.compute-1.amazonaws.com:5432/d80430r61b06e8";
-		String user = "xnvixqoqtrajjd";
-		String password = "8a71219d08a848290aa153e3af48d9f4c0a82bcc78f9d9c8e686ad918655b424";
-		String driverName = "org.postgresql.Driver"; //depende del motor 
+		String url = System.getenv("DATABASE_URL");//"jdbc:postgresql://ec2-52-203-164-61.compute-1.amazonaws.com:5432/d80430r61b06e8";
+		String user = System.getenv("SPRING_DATASOURCE_USERNAME");;
+		String password = System.getenv("SPRING_DATASOURCE_PASSWORD");
+		String driverName = System.getenv("SPRING_DATASOURCE_DRIVER"); //depende del motor 
 		//es el paquete donde esta el driver
 		//en la dependencia MAVEN
 		//en este caso podemos usar los dos paquetes que tienen jdbc. 
